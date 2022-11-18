92dfec99201b0e4767c6c1fb082fb6084c59c9e2
Russell Blickhan <rwblickhan@gmail.com>
2022-11-17 17:17:11 -0800

    Initial Commit

---
+++ Rocky/RockyApp.swift
@@ -0,0 +1,20 @@
+//
+//  RockyApp.swift
+//  Rocky
+//
+//  Created by Russell Blickhan on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct RockyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}

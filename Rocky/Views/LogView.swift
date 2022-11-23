90d552de3ea8a190d7fbeabbad5a395eaa72dadf
Russell Blickhan <rwblickhan@gmail.com>
2022-11-23 15:47:05 -0800

    Implement edit button on log

--- Rocky/Views/LogView.swift
+++ Rocky/Views/LogView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct LogView: View {
+    @Environment(\.managedObjectContext) private var viewContext
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Climb.timestamp, ascending: false)],
         animation: .default)
@@ -23,12 +24,25 @@ struct LogView: View {
                         Text(climb.timestamp?.formatted() ?? "null")
                     }
                 }
+                .onDelete(perform: deleteItems)
             }
             .navigationTitle("Climb Log")
-//            .toolbar {
-//                ToolbarItem(placement: .navigationBarLeading) { editButton }
-//                ToolbarItem(placement: .navigationBarTrailing) { addButton }
-//            }
+            .toolbar { ToolbarItem(placement: .navigationBarTrailing) { EditButton() } }
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { climbs[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
         }
     }
 }

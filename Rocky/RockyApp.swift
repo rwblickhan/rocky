124f34ed00a22a54b8f89a707d6c16e851142b46
Russell Blickhan <rwblickhan@gmail.com>
2022-11-27 15:11:33 -0800

    Add Sentry

--- Rocky/RockyApp.swift
+++ Rocky/RockyApp.swift
@@ -5,11 +5,31 @@
 //  Created by Russell Blickhan on 11/17/22.
 //
 
+import Sentry
 import SwiftUI
 
 @main
 struct RockyApp: App {
     let persistenceController = PersistenceController.shared
+
+    init() {
+        SentrySDK.start { options in
+            options
+                .dsn =
+                "https://ec56232ad63b4c41851b5d1ab88799c1@o4504233599172608.ingest.sentry.io/4504233600155648"
+            options.debug = true // Enabled debug when first installing is always helpful
+
+            // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+            // We recommend adjusting this value in production.
+            options.tracesSampleRate = 1.0
+
+            // Features turned off by default, but worth checking out
+            options.enableAppHangTracking = true
+            options.enableFileIOTracking = true
+            options.enableCoreDataTracking = true
+            options.enableCaptureFailedRequests = true
+        }
+    }
 
     var body: some Scene {
         WindowGroup {
